acac5f950e0898816fbea8e3967376bf6c90cb27
XPortellCanal <xavier.portell.canal@gmail.com>
2024-04-09 22:55:42 +0200

    First Commit

---
+++ M2C3_Python_Assignment.py
@@ -0,0 +1,50 @@
+# Exercise 1
+size_name='Large'
+print(size_name)
+
+num_one=6
+print(num_one)
+
+working_days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+print(working_days)
+
+is_odd=True
+print(is_odd)
+
+# Exercise 2
+first_characters=size_name[0:3]
+print(first_characters)
+
+# Exercise 3
+first_element=working_days[0]
+print(first_element)
+
+# Exercise 4
+num_two=num_one+10
+print(num_two)
+
+
+# Exercise 5
+last_element=working_days[-1]
+print(last_element)
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_string=names.split(',')
+print(names_string)
+
+#Exercise 7: 
+new_string=names_string[0].upper()+names[5:]
+print(new_string)
+
+#Exercise 8:
+sentence=f"Back in the old days, when I was {num_one}"
+print(sentence)
+
+#Exercise 9:
+print("hello world")
+
+#Exercise 10 (Ejercicio extra):
+string="Hola que tal"
+print(string.replace("Hola","adiós"))
+ 
